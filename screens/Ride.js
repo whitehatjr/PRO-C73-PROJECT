f9f17759daeeaec90cb22d0248379d16e6729818
vishalgaddam873 <vishal18@navgurukul.org>
2021-06-19 06:01:12 +0530

    Name Changed

--- screens/Ride.js
+++ screens/Ride.js
@@ -288,7 +288,7 @@ export default class RideScreen extends Component {
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
         <View style={styles.upperContainer}>
           <Image source={appIcon} style={styles.appIcon} />
-          <Text style={styles.title}>E-Ride</Text>
+          <Text style={styles.title}>e-ride</Text>
           <Text style={styles.subtitle}>A Eco-Friendly Ride</Text>
         </View>
         <View style={styles.lowerContainer}>
